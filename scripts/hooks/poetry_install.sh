6e29fb0e7d9c1b022ed21fd246962d7b0f6c2bf4
winston-srp <winston@srp.one>
2024-12-16 16:16:55 +0800

    update version

--- scripts/hooks/poetry_install.sh
+++ scripts/hooks/poetry_install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 if git diff --cached --name-only | grep -q 'pyproject\.toml'; then
     echo "pyproject.toml has changed. Updating lockfile and installing dependencies."
     poetry lock
-    poetry install
-
-    git add .
-    git commit -m "Install dependencies using poetry"
+    poetry install -v
+else
+    echo "No changes in pyproject.toml. Skipping dependency installation."
 fi
