cdff8b8c1c8483e90b2e6ccb8077f20f8e0f02f8
winston-srp <winston@srp.one>
2024-12-28 10:20:12 +0800

    update deserialize

--- scripts/hooks/poetry_install.sh
+++ scripts/hooks/poetry_install.sh
@@ -2,7 +2,7 @@
 if git diff --cached --name-only | grep -q 'pyproject\.toml'; then
     echo "pyproject.toml has changed. Updating lockfile and installing dependencies."
     poetry lock
-    poetry install -v
+    poetry install
 else
     echo "No changes in pyproject.toml. Skipping dependency installation."
 fi
